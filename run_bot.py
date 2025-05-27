380ddf3ce94c6a0a89c849940e92ea636dc981c8
Sasha <69010894+ArmSasha@users.noreply.github.com>
2025-05-27 15:18:15 +0400

    Update run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -10,6 +10,22 @@ bot = Bot(token='7438789045:AAHOrtnYTttADfOAYsT6mPC0i0pV0WfHRJc')
 # Создание диспетчера без передачи бота
 dp = Dispatcher()
 
+# Хэндлер для команды /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    # Создаем инлайн-клавиатуру с кнопкой
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        types.InlineKeyboardButton(
+            text="Открыть Mini App", 
+            web_app=types.WebAppInfo(url="https://finory.onrender.com/")
+        )
+    )
+    
+    await message.answer(
+        "Привет! Нажми кнопку ниже, чтобы открыть Mini App:",
+        reply_markup=builder.as_markup()
+    )
 
 async def on_startup():
     print('Бот вышел в онлайн')
