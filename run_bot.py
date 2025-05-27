1257fa5759a3c7be3f8d7bc91422fbc01e1eba21
Sasha <69010894+ArmSasha@users.noreply.github.com>
2025-05-27 15:19:31 +0400

    Update run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -1,7 +1,8 @@
 import logging
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 import asyncio
-
 logging.basicConfig(level=logging.INFO)
 
 # Создание объекта бота
